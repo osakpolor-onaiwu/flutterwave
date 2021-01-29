64af5a7befdba3df0f20945d3c3b52058a37f1a5
onaiwu Osakpolor <onaiwuosakpolor@gmail.com>
2021-01-29 22:50:55 +0100

    deployed

--- server.js
+++ server.js
@@ -32,11 +32,13 @@ mongoose
         useCreateIndex: true,
         useUnifiedTopology: true,
     })
-    .then(() => console.log("db connected to mongodb"))
+    .then(() => {
+        console.log("db connected to mongodb");
+
+        const port = process.env.PORT || 5000;
+
+        app.listen(port, () => {
+            console.log(`Server started on port${port}`);
+        });
+    })
     .catch((err) => console.log(err));
-
-const port = process.env.port || 5000;
-
-app.listen(port, () => {
-    console.log(`Server started on port${port}`);
-});
